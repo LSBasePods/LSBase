8766f07af91bcea877e81657754923e396cec725
liulihui <liulihui@21cake.com>
2016-02-18 16:54:43 +0800

    添加获取当前显示controller

---
+++ LSUIKit/NSObject+LSTopViewController.h
@@ -0,0 +1,16 @@
+//
+//  NSObject+LSTopViewController.h
+//  LSBaseExample
+//
+//  Created by liulihui on 16/2/18.
+//  Copyright © 2016年 BasePod. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface NSObject (LSTopViewController)
+
++ (UIViewController *)topViewControllerInWindow;
+
+@end
---
+++ LSUIKit/NSObject+LSTopViewController.m
@@ -0,0 +1,32 @@
+//
+//  NSObject+LSTopViewController.m
+//  LSBaseExample
+//
+//  Created by liulihui on 16/2/18.
+//  Copyright © 2016年 BasePod. All rights reserved.
+//
+
+#import "NSObject+LSTopViewController.h"
+
+@implementation NSObject (LSTopViewController)
+
++ (UIViewController *)topViewControllerInWindow
+{
+    UIWindow *windows = [[UIApplication sharedApplication].windows firstObject];
+    UIViewController *topViewController = [UIViewController returnVisibleViewControllerForController:windows.rootViewController];
+    return topViewController;
+}
+
++ (UIViewController *)returnVisibleViewControllerForController:(UIViewController *)controller {
+    if ([controller presentedViewController]) {
+        return [self returnVisibleViewControllerForController:[controller presentedViewController]];
+    } else if ([controller isKindOfClass:[UITabBarController class]]) {
+        return [self returnVisibleViewControllerForController:[(UITabBarController *)controller selectedViewController]];
+    } else if ([controller isKindOfClass:[UINavigationController class]]) {
+        return [self returnVisibleViewControllerForController:[(UINavigationController *)controller topViewController]];
+    } else {
+        return controller;
+    }
+}
+
+@end
